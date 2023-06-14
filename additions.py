313fb9b906abb147934e675fdd73beea207142e4
Kaushik Tummalapalli <54365583+kaushik-42@users.noreply.github.com>
2023-06-14 17:43:57 -0400

    Update additions.py

--- additions.py
+++ additions.py
@@ -23,60 +23,24 @@ tshirt_sizes = {
     "sp-6xl": 768
 }
 
-# Create an empty dataframe to store the results
-result_df = pd.DataFrame(columns=["Use Case", "Month", "Compute Cost", "Compute Volume",
-                                  "Storage Costs", "Prod Storage Volume", "Non-Prod Storage Volume"])
+# Create an empty DataFrame to store the use case details
+use_case_table = pd.DataFrame(columns=["Use Case", "T-Shirt Size", "Weekdays Hours/Day", "Weekends Hours/Day",
+                                       "Production Storage Volume", "Production Growth",
+                                       "Non-Production Storage Volume", "Non-Production Growth"])
 
-st.title("Cloud Cost Estimation")
-st.header("Use Cases")
+# Function to add a new row to the use case table
+def add_row():
+    new_row = {"Use Case": "", "T-Shirt Size": "", "Weekdays Hours/Day": 0.0, "Weekends Hours/Day": 0.0,
+               "Production Storage Volume": 0.0, "Production Growth": 0.0,
+               "Non-Production Storage Volume": 0.0, "Non-Production Growth": 0.0}
+    use_case_table.loc[len(use_case_table)] = new_row
 
-# Create a table to store use case details
-use_case_table = pd.DataFrame(columns=["Use Case", "T-Shirt Size", "Weekday Hours", "Weekend Hours",
-                                       "Prod Storage Volume", "Non-Prod Storage Volume"])
+# Function to remove a row from the use case table
+def remove_row(index):
+    use_case_table.drop(index, inplace=True)
+    use_case_table.reset_index(drop=True, inplace=True)
 
-# Add a default row to the use case table
-use_case_table = use_case_table.append({"Use Case": "", "T-Shirt Size": "", "Weekday Hours": 8.0,
-                                        "Weekend Hours": 0.0, "Prod Storage Volume": 1.0,
-                                        "Non-Prod Storage Volume": 1.0}, ignore_index=True)
-
-# Function to calculate costs for all use cases
-def calculate_costs():
-    # Iterate through the use case table
-    for index, row in use_case_table.iterrows():
-        use_case = row["Use Case"]
-        tshirt_size = row["T-Shirt Size"]
-        weekdays_hours_per_day = row["Weekday Hours"]
-        weekends_hours_per_day = row["Weekend Hours"]
-        Production_Storage_Volume = row["Prod Storage Volume"]
-        NonProd_Storage_Volume = row["Non-Prod Storage Volume"]
-
-        # Calculate the credits per hour based on the selected t-shirt size
-        credits_per_hour = tshirt_sizes.get(tshirt_size)
-
-        # Calculate the total credits per month
-        total_credits = credits_per_hour * ((weekdays_hours_per_day * 21.7) + (weekends_hours_per_day * 8.7))
-
-        # Calculate Direct Costs for each month and store in the result dataframe
-        monthly_costs = []
-        for month in range(1, 13):
-            # Calculate Compute Cost
-            compute_cost = total_credits * 2.5
-
-            # Calculate Storage Costs
-            production_storage_cost_per_month = 20 * Production_Storage_Volume
-            nonprod_storage_cost_per_month = 20 * NonProd_Storage_Volume
-
-            # Add the monthly costs to the list
-            monthly_costs.append({
-                "Use Case": use_case,
-                "Month": f"Month {month}",
-                "Compute Cost": compute_cost,
-                "Compute Volume": total_credits,
-                "Storage Costs": production_storage_cost_per_month + nonprod_storage_cost_per_month,
-                "Prod Storage Volume": Production_Storage_Volume,
-                "Non-Prod Storage Volume": NonProd_Storage_Volume})
-            
-# Function to calculate costs for all use cases
+# Function to calculate costs based on the use case details
 def calculate_costs():
     result_df = pd.DataFrame(columns=["Use Case", "Month", "Compute Cost", "Compute Volume",
                                       "Storage Costs", "Prod Storage Volume", "Non-Prod Storage Volume"])
@@ -86,75 +50,67 @@ def calculate_costs():
         tshirt_size = row["T-Shirt Size"]
         weekdays_hours_per_day = row["Weekdays Hours/Day"]
         weekends_hours_per_day = row["Weekends Hours/Day"]
-        Production_Storage_Volume = row["Production Storage Volume"]
-        Production_Growth = row["Production Growth"]
-        NonProd_Storage_Volume = row["Non-Production Storage Volume"]
-        NonProd_Growth = row["Non-Production Growth"]
+        production_storage_volume = row["Production Storage Volume"]
+        production_growth = row["Production Growth"]
+        non_production_storage_volume = row["Non-Production Storage Volume"]
+        non_production_growth = row["Non-Production Growth"]
 
+        # Calculate costs for each use case
         monthly_costs = []
         for month in range(1, 13):
             credits_per_hour = tshirt_sizes.get(tshirt_size)
             total_credits = credits_per_hour * ((weekdays_hours_per_day * 21.7) + (weekends_hours_per_day * 8.7))
             compute_cost = total_credits * 2.5
 
-            production_storage_cost_per_month = 20 * Production_Storage_Volume * (1 + Production_Growth / 100)
-            nonprod_storage_cost_per_month = 20 * NonProd_Storage_Volume * (1 + NonProd_Growth / 100)
+            production_storage_cost_per_month = 20 * production_storage_volume * (1 + production_growth / 100)
+            non_production_storage_cost_per_month = 20 * non_production_storage_volume
+            non_production_storage_cost_per_month = 20 * non_production_storage_volume * (1 + non_production_growth / 100)
+
+            total_direct_costs = compute_cost + production_storage_cost_per_month + non_production_storage_cost_per_month
 
             monthly_costs.append({
                 "Use Case": use_case,
                 "Month": f"Month {month}",
                 "Compute Cost": compute_cost,
                 "Compute Volume": total_credits,
-                "Storage Costs": production_storage_cost_per_month + nonprod_storage_cost_per_month,
-                "Prod Storage Volume": Production_Storage_Volume,
-                "Non-Prod Storage Volume": NonProd_Storage_Volume
+                "Storage Costs": production_storage_cost_per_month + non_production_storage_cost_per_month,
+                "Prod Storage Volume": production_storage_volume,
+                "Non-Prod Storage Volume": non_production_storage_volume
             })
 
         result_df = result_df.append(monthly_costs, ignore_index=True)
 
     return result_df
 
-# Main code
-st.title("Cloud Cost Estimation")
-st.header("Use Cases")
 
-use_case_table = pd.DataFrame(columns=["Use Case", "T-Shirt Size", "Weekdays Hours/Day",
-                                       "Weekends Hours/Day", "Production Storage Volume",
-                                       "Production Growth", "Non-Production Storage Volume",
-                                       "Non-Production Growth"])
+# Streamlit app
+def main():
+    st.title("Cloud Cost Estimation")
+    st.header("Use Cases")
 
-add_row()
-result_df = pd.DataFrame(columns=["Use Case", "Month", "Compute Cost", "Compute Volume",
-                                  "Storage Costs", "Prod Storage Volume", "Non-Prod Storage Volume"])
-
-# Display the use case table
-st.subheader("Use Case Details")
-st.dataframe(use_case_table)
-
-# Add buttons to add or remove rows in the use case table
-if st.button("Add Use Case"):
+    # Add initial row
     add_row()
 
-if len(use_case_table) > 1:
-    remove_indices = []
-    for index, row in use_case_table.iterrows():
-        if st.button(f"Remove Use Case {index+1}"):
-            remove_indices.append(index)
-    for index in remove_indices:
-        remove_row(index)
+    # Display the use case table
+    st.table(use_case_table)
 
-# Calculate the costs for all use cases
-if st.button("Calculate"):
-    result_df = calculate_costs()
+    # Add a new row button
+    if st.button("Add Row"):
+        add_row()
 
-# Display the result dataframe
-st.subheader("Results")
-st.dataframe(result_df.set_index('Month'))
+    # Remove a row button
+    if st.button("Remove Row"):
+        selected_rows = st.multiselect("Select Rows to Remove", list(use_case_table.index))
+        for row in selected_rows:
+            remove_row(row)
 
-# Check if the user wants to clear the result dataframe
-if st.button("Clear Results"):
-    result_df = pd.DataFrame(columns=["Use Case", "Month", "Compute Cost", "Compute Volume",
-                                      "Storage Costs", "Prod Storage Volume", "Non-Prod Storage Volume"])
+    # Calculate costs button
+    if st.button("Calculate Costs"):
+        result_df = calculate_costs()
+        st.subheader("Results")
+        st.dataframe(result_df.set_index('Month'))
 
-# Run the Streamlit app
-st.write("Use case details and results have been successfully processed!")
+
+if __name__ == "__main__":
+    main()
+
