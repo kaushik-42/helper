a5b211267b0c43d57487cd6cbadfd1baa101beb3
Kaushik Tummalapalli <54365583+kaushik-42@users.noreply.github.com>
2023-06-14 11:43:09 -0400

    Update ts.py

--- ts.py
+++ ts.py
@@ -30,3 +30,53 @@ for application in unique_applications:
 
         # Evaluate the model's performance
         # Compare predictions with actual values and calculate evaluation metrics
+   
+# FOR XGBOOST:
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+
+# Step 1: Load and preprocess the data
+df = pd.read_csv('your_dataset.csv', parse_dates=['Date'], index_col='Date')
+# Perform any necessary preprocessing steps, handle missing values or outliers
+
+# Step 2: Separate the data by application
+unique_applications = df['Tag_fo_application'].unique()
+
+# Step 3: Perform XGBoost modeling for each application
+for application in unique_applications:
+    # Filter data for the current application
+    application_data = df[df['Tag_fo_application'] == application]['direct_cost']
+
+    # Perform manual train-test split
+    train_data, test_data = train_test_split(application_data, test_size=0.2)
+
+    # Check if train_data or test_data has insufficient data points
+    if len(train_data) < 2 or len(test_data) < 1:
+        print(f"Not enough data for {application}. Skipping...")
+        continue
+
+    try:
+        # Prepare the data
+        X_train = np.array(range(len(train_data))).reshape(-1, 1)
+        y_train = train_data.values
+
+        X_test = np.array(range(len(train_data), len(train_data) + len(test_data))).reshape(-1, 1)
+        y_test = test_data.values
+
+        # Train the XGBoost model
+        model = xgb.XGBRegressor()
+        model.fit(X_train, y_train)
+
+        # Make predictions on the test data
+        predictions = model.predict(X_test)
+
+        # Calculate RMSE
+        rmse = mean_squared_error(y_test, predictions, squared=False)
+        print(f"RMSE for {application}: {rmse:.2f}")
+
+    except Exception as e:
+        print(f"Error occurred for {application}: {str(e)}")
+
