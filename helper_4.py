7745e4db5f280244cb3cbe8a8f7f3a19454241fe
Kaushik Tummalapalli <54365583+kaushik-42@users.noreply.github.com>
2023-07-16 17:53:28 -0400

    Update helper_4.py

--- helper_4.py
+++ helper_4.py
@@ -64,3 +64,66 @@ table = get_actuals_forecasts_table(application, year)
 
 # Print the table
 print(table)
+
+
+#######################
+# Handling More Use cases:
+import pandas as pd
+
+# Function to get the actuals and forecasts table
+def get_actuals_forecasts_table(application, year):
+    model = best_models.get(application)
+    if model is None:
+        print(f"No model found for application: {application}")
+        return None
+
+    application_data = df[df['Tag_fo_application'] == application][['Date', 'direct_cost']]
+    application_data = application_data.rename(columns={'Date': 'ds', 'direct_cost': 'y'})
+
+    # Filter data for the specified year
+    year_data = application_data[application_data['ds'].dt.year == year]
+
+    # Generate monthly dates for aggregation
+    monthly_dates = pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31', freq='M')
+
+    # Initialize empty lists for actuals and forecasts
+    actuals = []
+    forecasts = []
+    is_actual = []
+
+    # Aggregate weekly predictions into monthly actuals and forecasts
+    for month_start in monthly_dates:
+        month_end = month_start + pd.offsets.MonthEnd()
+        month_data = year_data[(year_data['ds'] >= month_start) & (year_data['ds'] <= month_end)]
+        
+        if len(month_data) > 0:
+            # Use actuals if available for the month
+            actuals.append(month_data['y'].sum())
+            forecasts.append(None)  # Placeholder for forecasts since actuals are available
+            is_actual.append(True)  # Mark as actual
+        elif len(actuals) > 0:
+            # Use forecast for the month
+            month_forecast = model.predict(pd.DataFrame({'ds': [month_start]}))
+            forecasts.append(month_forecast['yhat'].values[0])
+            actuals.append(None)  # Placeholder for actuals since forecasts are used
+            is_actual.append(False)  # Mark as forecast
+        else:
+            # No data available, fill with None for actuals and forecasts
+            actuals.append(None)
+            forecasts.append(None)
+            is_actual.append(False)
+    
+    # Create the table
+    table = pd.DataFrame({'Month': monthly_dates, 'Actuals': actuals, 'Forecasts': forecasts, 'IsActual': is_actual})
+    
+    return table
+
+# Example usage
+application = 'Application A'
+year = 2023
+
+table = get_actuals_forecasts_table(application, year)
+
+# Print the table
+print(table)
+
